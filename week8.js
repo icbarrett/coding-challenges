f263c495f64928a4bd64189baae89e4d1fa14308
Ingrid Barrett <barrett92@yahoo.com>
2022-06-13 14:12:45 -0400

    week 8 coding challenge

---
+++ week8.js
@@ -0,0 +1,30 @@
+
+// Create a function that takes two strings as arguments and returns the number of times the first string (the single character) is found in the second string.
+
+// Examples
+
+// charCount("a", "edabit") ➞ 1
+
+// charCount("c", "Chamber of secrets") ➞ 1
+
+// charCount("b", "big fat bubble") ➞ 4
+// Notes
+
+// Your output must be case-sensitive (see second example).
+
+
+function charCount(myChar, str) {
+	let count = 0;
+	for (let i = 0; i <= str.length; i++) {
+        if(str[i] === myChar) {
+		// if (myChar == str[i]) {
+			count+= 1;
+		}
+	}
+	return count;
+}
+
+console.log(charCount("a", "edabit")); 
+console.log(charCount("c", "Chamber of secrets")); 
+console.log(charCount("b", "big fat bubble")); 
+
